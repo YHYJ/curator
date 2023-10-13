20368903664d00f103e4ed796dfaf4cb37b616a6
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-10-13 09:56:56 +0800

    E: function/git_operation.go —— 新增本地仓库根据远程分支创建本地分支的函数

--- function/git_operation.go
+++ function/git_operation.go
@@ -15,6 +15,8 @@ import (
 	"io/fs"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 )
 
@@ -41,7 +43,7 @@ func IsLocalRepo(path string) (bool, *git.Repository) {
 	return true, repo
 }
 
-// 输出本地仓库[本地|远程]分支信息
+// 获取本地仓库[本地|远程]分支信息
 func GetRepoBranchInfo(worktree *git.Worktree, which string) ([]fs.FileInfo, error) {
 	var branchDir string
 	switch which {
@@ -60,7 +62,39 @@ func GetRepoBranchInfo(worktree *git.Worktree, which string) ([]fs.FileInfo, err
 	return branchs, nil
 }
 
-// 输出本地仓库子模块信息
+// 本地仓库根据远程分支refs/remotes/origin/<remoteBranchName>创建本地分支refs/heads/<localBranchName>
+func CreateLocalBranch(repo *git.Repository, branchs []fs.FileInfo) {
+	for _, branch := range branchs {
+		fmt.Println(branch.Name(), "---->")
+		// 修改.git/config，增加新的分支配置
+		branchReferenceName := plumbing.NewBranchReferenceName(branch.Name()) // 构建本地分支Reference名，格式：refs/heads/<localBranchName>
+		err := repo.CreateBranch(&config.Branch{                              // 分支配置写入.git/config
+			Name:   branch.Name(),
+			Remote: "origin",
+			Merge:  branchReferenceName,
+		})
+		if err != nil {
+			fmt.Printf("\x1b[31m%s\x1b[0m\n", "Branch configuration already exists in .git/config")
+		}
+
+		// 创建一个新的Reference
+		remote := "origin"                                                             // 远程名称
+		newBranchReferenceName := plumbing.ReferenceName(branchReferenceName.String()) // refs/heads/test
+		remoteReferenceName := plumbing.NewRemoteReferenceName(remote, branch.Name())  // 构建远程分支Reference名，格式：refs/remotes/origin/<remoteBranchName>
+		remoteReferenceData, err := repo.Reference(remoteReferenceName, true)          // 根据远程分支Reference名获取其Hash值，格式：1a8f900411d35a620407ce07902aecadfc782ded refs/remotes/origin/test
+		if err != nil {
+			fmt.Printf("\x1b[31m%s\x1b[0m\n", err)
+			continue
+		}
+		newReference := plumbing.NewHashReference(newBranchReferenceName, remoteReferenceData.Hash()) // 基于Hash创建新的Reference
+		if err = repo.Storer.SetReference(newReference); err != nil {                                 // 写入新Reference
+			fmt.Printf("\x1b[31m%s\x1b[0m\n", err)
+			continue
+		}
+	}
+}
+
+// 获取本地仓库子模块信息
 func GetLocalRepoSubmoduleInfo(worktree *git.Worktree) (git.Submodules, error) {
 	submodules, err := worktree.Submodules()
 	if err != nil {
