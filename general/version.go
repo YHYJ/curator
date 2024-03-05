81ca5eb5ddaefc4e8707cd2644a4195595cc5005
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-03-05 09:16:49 +0800

    v0.9.0

--- general/version.go
+++ general/version.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	Name    string = "Repos"                 // 程序名
-	Version string = "v0.8.2"                // 程序版本
+	Version string = "v0.9.0"                // 程序版本
 	Project string = "github.com/yhyj/repos" // 项目地址
 )
 
