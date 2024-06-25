3347ac7fd4885504c3d155b93843cdbe59e0b5d0
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-06-25 16:54:18 +0800

    v1.2.7

--- general/version.go
+++ general/version.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	Name    string = "Curator"                 // 程序名
-	Version string = "v1.2.6"                  // 程序版本
+	Version string = "v1.2.7"                  // 程序版本
 	Project string = "github.com/yhyj/curator" // 项目地址
 )
 
