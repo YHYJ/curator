739f04014882c96dfc09224f8fe51ac780bdbfe2
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-12-30 10:38:50 +0800

    用户确认函数升级

--- general/define_other.go
+++ general/define_other.go
@@ -26,41 +26,50 @@ func Delay(second float32) {
 	time.Sleep(time.Duration(second*1000) * time.Millisecond)
 }
 
-// AskUser 询问用户
+// AreYouSure 获取用户二次确认
 //
 // 参数：
 //   - question: 问题
-//   - standardAnswers: 期望回答的切片（最后一个选项是默认值），例如 [y, N] 代表期望输入 y 或 n，最后一个选项是默认值（大写为了提示用户其为默认值）
+//   - defaultAnswer: 默认回答，true 或 false
 //
 // 返回：
-//   - 回答
+//   - true/false
 //   - 错误信息
-func AskUser(question string, standardAnswers []string) (string, error) {
-	viewAnswers := strings.Join(standardAnswers, "/")
-	color.Printf("%s %s: ", question, SecondaryText("(", viewAnswers, ")"))
+func AreYouSure(question string, defaultAnswer bool) (bool, error) {
+	var (
+		viewAnswers []string                                 // 显示用可选答案
+		answersMap  = map[string]bool{"y": true, "n": false} // 可选答案和实际返回值的映射
+		reader      = bufio.NewReader(os.Stdin)              // 标准输入
+	)
 
-	// 默认回答
-	var answer = strings.ToLower(standardAnswers[len(standardAnswers)-1])
-
-	// 从标准输入中读取用户的回答
-	reader := bufio.NewReader(os.Stdin)
-	userRawAnswer, err := reader.ReadString('\n')
-	if err != nil {
-		return "", err
+	// 根据 defaultAnswer 设置显示用的可选答案
+	if defaultAnswer == true {
+		viewAnswers = []string{"Y", "n"}
+	} else {
+		viewAnswers = []string{"y", "N"}
 	}
+	viewAnswersConsortium := strings.Join(viewAnswers, "/") // 显示用可选答案的组合体
 
-	// 用户回答不为空则符合要求，转为小写
-	userAnswer := strings.TrimSpace(strings.TrimSuffix(userRawAnswer, "\n"))
-	if len(userAnswer) != 0 {
-		answer = strings.ToLower(userAnswer)
-	}
+	for {
+		// 输出问题
+		color.Printf("%s %s: ", question, SecondaryText("(", viewAnswersConsortium, ")"))
 
-	// 检测输入是否符合要求，不符合则返回默认值
-	for _, standardAnswer := range standardAnswers {
-		if answer == standardAnswer {
-			return answer, nil
+		// 从标准输入中读取用户的回答
+		userRawAnswer, err := reader.ReadString('\n')
+		if err != nil {
+			return false, err
+		}
+
+		// 去除用户回答中的换行符
+		userAnswer := strings.TrimSpace(strings.TrimSuffix(userRawAnswer, "\n"))
+
+		// 检查用户回答是否符合要求
+		for answer, result := range answersMap {
+			if strings.EqualFold(userAnswer, answer) {
+				return result, nil
+			} else if userAnswer == "" { // 如果用户回答为空，返回默认回答
+				return defaultAnswer, nil
+			}
 		}
 	}
-
-	return answer, nil
 }
